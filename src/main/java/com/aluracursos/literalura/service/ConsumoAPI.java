d90d2ea3c7748886afaa4065c2e922b1e4393ed5
Nelvi Castellanos <nelvicas97@gmail.com>
2025-12-29 21:45:24 -0600

    contruyendo solicitud a la API

--- src/main/java/com/aluracursos/literalura/service/ConsumoAPI.java
+++ src/main/java/com/aluracursos/literalura/service/ConsumoAPI.java
@@ -1,4 +1,36 @@
 package com.aluracursos.literalura.service;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ConsumoAPI {
+
+    // Metodo   conexion a la api y ver datos en formato json
+
+
+    public String obtenerDatos(String url){
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        String json = response.body();
+        return json;
+
+    }
 }
+
+
